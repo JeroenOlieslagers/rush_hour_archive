ba8e6dbc9c8f3f2a4a320ded5517c8b985b8e214
JeroenOlieslagers <jolieslagers@gmail.com>
2022-10-14 09:05:00 -0400

    Linear regression analysis

--- analysis/src/puzzle_analysis.jl
+++ analysis/src/puzzle_analysis.jl
@@ -3,49 +3,35 @@ include("solvers.jl")
 using FileIO
 using Distributions
 using StatsPlots
+using MixedModels
+using StatsModels
+using GLM
+using MLJLinearModels
+using ColorSchemes
+using LaTeXStrings
+using Combinatorics
+using MLDataUtils
 
-
-data = load("analysis/processed_data/filtered_data.jld2")["data"]
 
 """
     get_Ls(data)
 
 Return dictionary where keys are problem ids and values are arrays of lengths of people's solution.
 """
-function get_Ls(data::Dict{String, DataFrame})::DefaultDict{String, Array{Int64}, Vector{Any}}
-    dict = DefaultDict{String, Array{Int}}([])
+function get_Ls(data::Dict{String, DataFrame})::Tuple{DefaultDict{String, Array{Int64}, Vector{Any}}, DefaultDict{String, Dict{String, Int64}, Dict{Any, Any}}}
+    Ls = DefaultDict{String, Array{Int}}([])
+    dict = DefaultDict{String, Dict{String, Int}}(Dict())
     # Loop over all subjects
     for subj in keys(data)
         # Get L for each problem subject attempted
         _, _, attempts = analyse_subject(data[subj]);
         for prb in keys(attempts)
             # Push L to array for each problem
-            push!(dict[prb], attempts[prb])
+            push!(Ls[prb], attempts[prb])
+            dict[subj][prb] = attempts[prb]
         end
     end
-    return dict
-end
-
-
-"""
-    get_optimal_lens(prbs)
-
-Return dictionary where keys are problem ids and values are optimal lengths of solving that puzzle
-Also return dictionary where values are the optimal moves themselves
-"""
-function get_optimal_lens(prbs::Vector{String})::Tuple{Dict{String, Int}, Dict{String, Array{Array{Int, 1}}}}
-    dict = Dict{String, Int}()
-    opt_moves = Dict{String, Vector{Any}}()
-    #exps = 0
-    for prb in prbs
-        board = load_data(prb)
-        _, past_moves, exp = a_star(board, h=red_distance)
-        dict[prb] = length(past_moves)
-        opt_moves[prb] = past_moves
-        #exps += exp
-    end
-    #println(exps)
-    return dict, opt_moves
+    return Ls, dict
 end
 
 
@@ -68,7 +54,7 @@ returns all necessary features:
 10. maximum width of state space
 11. depth of state space
 """
-function get_state_spaces(prbs::Vector{String})
+function get_state_spaces(prbs::Vector{String})::Dict{String, Vector{Float64}}
     state_space_features = Dict{String, Vector{Float64}}()
     for prb in prbs
         println(prb)
@@ -135,7 +121,7 @@ get_random_agent_stats(prbs, N=100)
 Returns mean and standard deviation of moves a random agent performs to solve the problem
 over N iterations.
 """
-function get_random_agent_stats(prbs::Vector{String}; N=100)
+function get_random_agent_stats(prbs::Vector{String}; N=100)::Dict{String, Vector{Float64}}
     dict = Dict{String, Vector{Float64}}()
     for prb in prbs
         println(prb)
@@ -149,27 +135,153 @@ function get_random_agent_stats(prbs::Vector{String}; N=100)
     return dict
 end
 
+"""
+    get_dataframe(prbs, dict, state_space_features, random_agent_stats)
+
+Creates dataframe for each subject and puzzle index with each of 13 features as well as subject solution length.
+"""
+function get_dataframe(prbs::Vector{String}, dict::DefaultDict{String, Dict{String, Int64}, Dict{Any, Any}}, state_space_features::Dict{String, Vector{Float64}}, random_agent_stats::Dict{String, Vector{Float64}})
+    df = DataFrame(
+        subj=String[], 
+        prb=String[],
+        L=Int[],
+        opt_L=Int[], 
+        branch=Float64[], 
+        indegree=Float64[], 
+        outdegree=Float64[],
+        n_sol=Int[],
+        paths=Int[],
+        ss_size=Int[],
+        av_width=Float64[],
+        max_width=Int[],
+        min_width=Int[],
+        depth=Int[],
+        mean_random=Float64[],
+        std_random=Float64[])
+
+    for subject in keys(dict)
+        subj_prbs = dict[subject]
+        for prb in keys(subj_prbs)
+            push!(df, [subject, prb, subj_prbs[prb], state_space_features[prb]..., random_agent_stats[prb]...])
+        end
+    end
+    # Normalise variance
+    for column in names(df)[4:end]
+        df[!, column] ./= std(df[!, column])
+    end
     
-Ls = get_Ls(data)
-prbs = collect(keys(Ls))[sortperm([parse(Int, x[end-1] == '_' ? x[end] : x[end-1:end]) for x in keys(Ls)])]
-
-state_space_features = get_state_spaces(prbs)
-
-random_agent_stats = get_random_agent_stats(prbs)
-
-x = [[] for _ in 1:13]
-y = []
-for prb in prbs
-    for i in 1:11
-        push!(x[i], [state_space_features[prb][i] for _ in 1:length(Ls[prb])]...)
-    end
-    for i in 12:13
-        push!(x[i], [random_agent_stats[prb][i-11] for _ in 1:length(Ls[prb])]...)
-    end
-    push!(y, Ls[prb]...)
+    return df
 end
 
-scatter(x[12], y, legend=nothing)
+"""
+    lasso_analysis(XX, av_L)
+
+Fits lasso regressions for all features XX using range of lamba values.
+Also plots correlation heatmap.
+"""
+function lasso_analysis(XX, av_L)
+
+    lambdas = 10 .^(range(-3,stop=log10(2),length=10))
+
+    colors = ColorScheme([RGB(n/100, (100-n)/100, 0) for n in 1:100])
+    ps = []
+    for (n, l) in enumerate(lambdas)
+        en = LassoRegression(l)
+        p = MLJLinearModels.fit(en, XX, av_L)
+        push!(ps, p[1:end-1])
+    end
+    plot(ps, ylabel="Coefficient", xlabel="Feature", xticks=(1:13, names(df)[4:end]), xrotation=45, size=(400, 350), palette=colors, legend=nothing, colorbar=true, title="Lasso regression for different values of "*latexstring("\\lambda"), titlefontsize=10, dpi=400)
+    plot!([2, 2.001], [0, 0], zcolor=[0, lambdas[end]], color=palette(colors), dpi=400)
+
+    heatmap(abs.(cor(XX)), size=(400,450), yflip=true, xticks=(1:13, names(df)[4:end]), legend=nothing, xrotation=60, yticks=(1:13, names(df)[4:end]), tick_direction=:out, title="Absolute correlation coefficient", dpi=400, c=:greys)
+
+end
+
+"""
+    exhaustive_linear_bic(df)
+
+Fits a range of linear regression models over powerset of features. 
+Returns AIC, BIC and 5-fold cross validated log likelihood.
+"""
+function exhaustive_linear(df)
+    feature_sets = powerset([feature for feature in names(df) if feature ∉ ["subj", "prb", "L"]])
+    BICs = zeros(length(collect(feature_sets)))
+    AICs = zeros(length(collect(feature_sets)))
+    CVs = zeros(length(collect(feature_sets)))
+    for (n, feature_set) in enumerate(feature_sets)
+        # Skip empty set
+        if n == 1
+            continue
+        end
+        if n % 100 == 0
+            println(n)
+        end
+        fm = Term(:L) ~ term(1) +([Term(Symbol(name)) for name in feature_set]...)
+        M = fit(LinearModel, fm, df)
+        BICs[n] = bic(M)
+        AICs[n] = aic(M)
+        # 5 fold Cross Validation (CV)
+        folds = kfolds(shuffle(1:size(df)[1]), 5)
+        for fold in folds
+            # Fit on 80% of data
+            M = fit(LinearModel, fm, df[fold[1], :])
+            # Test on 20% of data
+            P = predict(M, df[fold[2], :])
+            y = df[fold[2], "L"]
+            CVs[n] += loglik(y, P)
+        end
+    end
+    return AICs, BICs, CVs
+end
+
+"""
+    loglik(y, P)
+
+Returns loglikelihood of linear model on predictions P and data y.
+"""
+function loglik(y::Vector, P::Vector)::Float64
+    dev = sum((y .- P).^2)
+    n = length(y)
+    return -n/2 * (log(2π * dev/n) + 1)
+end
+
+data = load("analysis/processed_data/filtered_data.jld2")["data"]
+
+Ls, dict = get_Ls(data)
+prbs = collect(keys(Ls))[sortperm([parse(Int, x[end-1] == '_' ? x[end] : x[end-1:end]) for x in keys(Ls)])]
+
+# state_space_features = get_state_spaces(prbs)
+
+# random_agent_stats = get_random_agent_stats(prbs)
+
+# df = get_dataframe(prbs, dict, state_space_features, random_agent_stats)
+
+X = load("analysis/processed_data/features.jld2")["data"]
+XX = load("analysis/processed_data/norm_features.jld2")["data"]
+
+df = DataFrame(CSV.File("analysis/processed_data/13_features.csv"))
+
+#df = DataFrame(load("analysis/processed_data/13_features.jld2"))
+#prbs = unique(df[!, "prb"])[sortperm([parse(Int, x[end-1] == '_' ? x[end] : x[end-1:end]) for x in unique(df[!, "prb"])])]
+
+#AICs, BICs, CVs = exhaustive_linear(df)
+#save("analysis/processed_data/exhaustive_linear_CVs.jld2", "data", CVs)
+BICs = load("analysis/processed_data/exhaustive_linear_BICs.jld2")["data"]
+BICs = load("analysis/processed_data/exhaustive_linear_AICs.jld2")["data"]
+BICs = load("analysis/processed_data/exhaustive_linear_CVs.jld2")["data"]
+
+fm = @formula(L ~ opt_L)
+fm = Term(:L) ~ (term(1) | Term(:subj)) +([Term(Symbol(name)) for name in names(df) if name ∉ ["subj", "prb", "L"]]...)
+M = fit(MixedModel, fm, df)
+
+M = fit(LinearModel, fm, df)
+
+av_L = [mean(Ls[prb]) for prb in prbs]
+
+
+
+en = ElasticNetRegression(0, 0)
+p = MLJLinearModels.fit(en, XX, av_L)
 
 X = zeros(70, 13)
 for (i, prb) in enumerate(prbs)
@@ -184,6 +296,37 @@ end
 
 XX = zeros(size(X))
 for i in 1:13
-    XX[:, i] = X[:, i] .- mean(X[:, i])
-    XX[:, i] = XX[:, i] / norm(XX[:, i])
+    #XX[:, i] = X[:, i] .- mean(X[:, i])
+    #XX[:, i] = XX[:, i] / norm(XX[:, i])
+    XX[:, i] = X[:, i] / std(X[:, i])
 end
+
+
+
+
+
+
+A = -0.5
+σ = 0.3
+dts = 10 .^(range(-3, 0, 100))
+
+
+function ou(dts::Vector{Float64}, A::Float64, σ::Float64)::Vector{Float64}
+    vars = zeros(length(dts))
+    for i in 1:length(dts)
+        v = randn(100000000)
+        x = zeros(100000000)
+        x[1] = randn()*σ/sqrt(-2*A)
+        adt = 1 + A*dts[i]
+        sdt = σ*sqrt(dts[i])
+        @inbounds for t in 1:(100000000-1)
+            x[t+1] = x[t]*adt + v[t]*sdt
+        end
+        vars[i] = var(x)
+    end
+    return vars
+end
+
+
+
+
