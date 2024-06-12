8d1d1234f706fb228613f91e4f29cfa03a0ccc54
JeroenOlieslagers <jolieslagers@gmail.com>
2024-06-12 13:16:53 -0400

    Add project details

--- src/__RushHour.jl
+++
@@ -1,3 +0,0 @@
-module RushHour
-
-end # module
--- src/main.jl
+++ src/main.jl
@@ -1,35 +1,4 @@
-begin
-using JSON
-using StaticArrays
-using BenchmarkTools
-using DataStructures
-using DataFrames
-using CSV
-using JLD2
-using StatsPlots
-using StatsBase
-using ProgressBars
-using Random
-using MLUtils
-using Plots
-using GraphViz
-using LaTeXStrings
-using Optim
-using LinearAlgebra
-using SparseArrays
-include("representations.jl")
-include("rush_hour.jl")
-include("visualization.jl")
-include("search.jl")
-include("pre_process.jl")
-include("data_analysis.jl")
-include("and_or_trees.jl")
-include("and_or_model.jl")
-include("models.jl")
-include("model_fitting.jl")
-include("model_validation.jl")
-include("plotting.jl")
-end
+using RushHour
 
 d_goals_prbs = load("data/processed_data/d_goals_prbs.jld2")
 
